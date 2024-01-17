2f32ca0350728dd42556983ac2b14b86b71cc1c6
adsweb4you <adsweb4you@gmail.com>
2024-01-17 08:26:13 +0400

    JS ARRAY

--- js/app.js
+++ js/app.js
@@ -48,5 +48,96 @@ function checkNaumber(str){
     console.log('ნაპოვნია რიცხვი');
  }
  
- 
+let array = [ 5, 'სტუდენტი2', 23, 'სტრინგი', 'ლექტორი', 55 ];
 
+// array.push('დამკვირვებელი'); // მასივში ახალი ელემენტის ჩამატება (ბოლოს)
+// array.unshift('კურატორი'); // მასივში ახალი ელემენტის ჩამატება (თავში)
+// array.pop();  // მასივში ახალი ელემენტის წაშლა (ბოლოს)
+// array.shift();  // მასივში ახალი ელემენტის წაშლა (თავში)
+// array.slice(2, -2) // მასივის ელემენტების წაშლა
+// array.splice(2, 2) // მასივის ელემენტების წაშლა
+//  array.splice(0, 0, 'ახალი ელემენტი', 'კიდევ ერთი ელემენტი'); // მასივის ელემენტების წაშლა/ჩამატება
+//  array.length ითვლის მასივში ელემენტების რაოდენობას
+let fn = array.includes('ლექტორი'); // ამოწმებს არსებობს თუ არა მასივში რაიმე ელემენტი
+
+// გამეორების მეთოდები
+
+// ამოწმებს მასივში ელემენტს თუ იპოვა 1 მაინც აბრუნებს boolean 
+let finds = array.some( function(element){
+  return  element == 'სტუდენტი';
+});
+
+// ამოწმებს მასივში ელემენტებს თუ ყველა წევრი აკმაყოფილებს პირობას აბრუნებს boolean 
+let findall = array.every( function(element){
+    return  element.length >= 1;
+ });
+
+ // ეძებს  მასივში ელემენტებს თუ იპოვა  აბრუნებს პირველივე წევრს
+let findel = array.find( function(element){
+    return  typeof element == 'string';
+ });
+
+  // ეძებს  მასივში ელემენტებს თუ იპოვა  აბრუნებს პირველივე წევრს
+let finall = array.filter( function(element){
+    return  typeof element == 'string';
+ });
+
+//  ბეჭდავს მასივის ყველა ელემენტს
+ let app = document.getElementById('arr');
+  array.forEach( function(el){
+      app.innerHTML  += `<h1> ${el} </h1>`;
+   } )
+
+ //  ბეჭდავს მასივის ყველა ელემენტს და ქმნის ახალ მასივს
+ let newarr =   array.map( function(el){
+     let leng;
+
+     if (typeof el == 'string') {
+        leng = el.length;
+     } else {
+        leng = ''
+     }
+
+     return `${el} შედგება ${leng} სიმბოლოსგან` 
+ } )
+
+let Users  = ['ედიტორი', 'ადმინ', 'მოდერატორი', ['ბლოკის უფროსი', 'ჰაი ლეველის ოპერატორი'] ];
+let Numbers  = [2, 150, 9098, 100000,34,55,7,67];
+
+// Users.sort();// მასივის სორტირება ა-ჰ
+Users.sort().reverse() // მასივის სორტირება ჰ-ა
+// რიცხვების სორტირება
+Numbers.sort( function(a,b){
+  return a - b;
+} );
+
+//let united = Users.concat(Numbers); // მასივების გაერთიანება
+let united =[...Users , ...Numbers]; // მასივების გაერთიანება
+ console.log(Users[3][0]);
+
+
+let outs = document.getElementById('finded');
+ function findsmy(value) {
+    outs.innerHTML = '';
+ let data = [
+    'შაკო',
+    'ზურა',
+    'ოთო',
+    'გიგი',
+    'ზურა',
+    'ლექსო'
+ ] 
+
+ let finds1 = data.filter(function (el){
+    return el.search(value) != -1;
+ })
+
+ if (finds1.length == 0) {
+    outs.innerHTML = 'მონაცემები ვერ მოიძებნა'
+ } else {
+     finds1.forEach(function(element){
+    outs.innerHTML += `<h1> ${element} </h1>`;
+ })
+ }
+
+ }
